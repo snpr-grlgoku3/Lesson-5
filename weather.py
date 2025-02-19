724b6d3d7dfaf4a004cb969019f440f62ba7078f
snpr_grlgoku3 <khyatibagga4@gmail.com>
2025-02-19 17:55:35 +0530

    Activity for lesson 5

---
+++ weather.py
@@ -0,0 +1,5 @@
+temperature= int(input("Please enter the temperature in your respective location: "))
+if temperature>30:
+    print("User according to the given temperature, light and airy clothes should be worn")
+else:
+    print("The weather is suitable for warm woollen clothes")
